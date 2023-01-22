3b16f9b4c74bf44524975a3be48bc957527d2916
albertkombol <kombolalbert@gmail.com>
2023-01-22 04:00:43 +0100

    Update styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from '@/styles/Footer.module.css';
 const Footer = () => {
     return(
         <footer>
-             <p className="by">Made with <span className={styles.hope}>hope</span> and <a href="#" target="_blank">NextJS</a></p>
+             <p className="mw">Made with <span className={styles.hope}>hope</span> and <a href="https://nextjs.org/learn/foundations/about-nextjs/what-is-nextjs" target="_blank">Next.js</a></p>
             <div className="cr">&copy; Copyright 2023 Simple Blog</div>
         </footer>
     )
